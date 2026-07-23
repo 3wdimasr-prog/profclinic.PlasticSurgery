3110020ecc0cc307eaf5859e41980a55cb9a7b39
3wdimasr-prog <3wdi.masr@gmail.com>
2026-07-23 19:01:13 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+/**
+ * ضع رابط Google Apps Script Web App بين علامتي الاقتباس بعد نشره.
+ * مثال: https://script.google.com/macros/s/AKfycb.../exec
+ */
+window.PROF_CLINIC_CONFIG = {
+  SHEETS_WEB_APP_URL: ""
+};
