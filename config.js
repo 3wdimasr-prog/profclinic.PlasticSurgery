6d301fd58d6655b6505412d9586dcdce6a03092d
3wdimasr-prog <3wdi.masr@gmail.com>
2026-07-23 19:44:03 +0300

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-/**
- * ضع رابط Google Apps Script Web App بين علامتي الاقتباس بعد نشره.
- * مثال: https://script.google.com/macros/s/AKfycb.../exec
- */
-window.PROF_CLINIC_CONFIG = {
-  SHEETS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbyBfTB_MjobmhVPGjztQiw3fCrn1dKR15nkncKVRKEJpRgC59ElpPqDKFn0pzxpTGnG/exec"
-};
